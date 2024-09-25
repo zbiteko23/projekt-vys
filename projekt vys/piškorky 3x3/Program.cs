81fbd3599cbb1f950e09afa268379478c0f968b9
Ondřej Zbitek <zbiteko23@spstrutnov.cz>
2024-09-25 13:58:55 +0200

    piškvorky 3x3

---
+++ projekt vys/piškorky 3x3/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Piskvorky
+{
+    class Program
+    {
+        static char[,] board = new char[3, 3];
+        static char currentPlayer = 'X';
+
+        static void Main(string[] args)
+        {
+            InitializeBoard();
+            PlayGame();
+        }
+
+        static void InitializeBoard()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = ' ';
+                }
+            }
+        }
+
+        static void PlayGame()
+        {
+            int moves = 0;
+            while (true)
+            {
+                PrintBoard();
+                Console.WriteLine($"Hráč {currentPlayer}, zadejte řádek (0-2) a sloupec (0-2):");
+                string input = Console.ReadLine();
+                var inputs = input.Split(',');
+
+                if (inputs.Length != 2 || !int.TryParse(inputs[0], out int row) || !int.TryParse(inputs[1], out int col) ||
+                    row < 0 || row > 2 || col < 0 || col > 2 || board[row, col] != ' ')
+                {
+                    Console.WriteLine("Neplatný vstup, zkuste to znovu.");
+                    continue;
+                }
+
+                board[row, col] = currentPlayer;
+                moves++;
+
+                if (CheckWin())
+                {
+                    PrintBoard();
+                    Console.WriteLine($"Hráč {currentPlayer} vyhrál!");
+                    break;
+                }
+                if (moves == 9)
+                {
+                    PrintBoard();
+                    Console.WriteLine("Hra skončila remízou!");
+                    break;
+                }
+
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            }
+        }
+
+        static void PrintBoard()
+        {
+            Console.Clear();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($" {board[i, 0]} | {board[i, 1]} | {board[i, 2]} ");
+                if (i < 2)
+                {
+                    Console.WriteLine("---|---|---");
+                }
+            }
+        }
+
+        static bool CheckWin()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                // Kontrola řádků a sloupců
+                if ((board[i, 0] == currentPlayer && board[i, 1] == currentPlayer && board[i, 2] == currentPlayer) ||
+                    (board[0, i] == currentPlayer && board[1, i] == currentPlayer && board[2, i] == currentPlayer))
+                {
+                    return true;
+                }
+            }
+
+            // Kontrola diagonál
+            if ((board[0, 0] == currentPlayer && board[1, 1] == currentPlayer && board[2, 2] == currentPlayer) ||
+                (board[0, 2] == currentPlayer && board[1, 1] == currentPlayer && board[2, 0] == currentPlayer))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ projekt vys/projekt vys/kámen nůžky papír.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace KamenNuzkyPapir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            string[] options = { "kámen", "nůžky", "papír" };
+            string userChoice;
+            string computerChoice;
+            string playAgain;
+
+            do
+            {
+                Console.WriteLine("Vyberte si: kámen, nůžky nebo papír:");
+                userChoice = Console.ReadLine().ToLower();
+
+                while (Array.IndexOf(options, userChoice) == -1)
+                {
+                    Console.WriteLine("Zkuste to znovu: kámen, nůžky nebo papír:");
+                    userChoice = Console.ReadLine().ToLower();
+                }
+
+                int computerChoiceIndex = random.Next(0, options.Length);
+                computerChoice = options[computerChoiceIndex];
+
+                Console.WriteLine($"Počítač si vybral: {computerChoice}");
+
+                if (userChoice == computerChoice)
+                {
+                    Console.WriteLine("Remíza!");
+                }
+                else if ((userChoice == "kámen" && computerChoice == "nůžky") ||
+                         (userChoice == "nůžky" && computerChoice == "papír") ||
+                         (userChoice == "papír" && computerChoice == "kámen"))
+                {
+                    Console.WriteLine("Vyhrál jsi!");
+                }
+                else
+                {
+                    Console.WriteLine("Prohrál jsi!");
+                }
+
+                Console.WriteLine("Chceš hrát znovu? (ano/ne)");
+                playAgain = Console.ReadLine().ToLower();
+
+            } while (playAgain == "ano");
+
+            Console.WriteLine("Dik za hru!");
+        }
+    }
+}
